b3eca47e3f9b044b9a7a18b6fdb0bc450b3cdf6e
Ulrich Hoffmann <uho@xlerb.de>
2014-03-19 14:14:45 +0100

    error handling, generation and location of named pipes

--- oberonlink1.py
+++ oberonlink1.py
@@ -12,9 +12,16 @@ import os
 import exceptions
 import time
 
-help_message = '''
-The help message goes here.
+help_message = '''\
+Usage: operonlink1.py [options] send|receive [filename]*
+
+options:
+   -h --help    this help
+   -v --verbose progress output\
 '''
+
+RISC_SERIAL_INPUT="/tmp/risc-serial-input"
+RISC_SERIAL_OUTPUT="/tmp/risc-serial-output"
 
 BlkLen = 255
 REQ = 0x20
@@ -32,32 +39,44 @@ def receive(risc_output):
             if e.errno!=35: # EAGAIN
                 raise
             time.sleep(0.1)
+            
+def send(risc_input, bytes):
+    while 1:
+        try:
+            os.write(risc_input, bytes)
+            return
+        except OSError, e:
+            if e.errno!=35: # EAGAIN
+                raise
+            time.sleep(0.1)
 
 def expectAck(risc_output):
     x=receive(risc_output)
     if x!=ACK:
         raise exceptions.Exception("ack expected",x)
 
-def sendFile((risc_input, risc_output), filename):
-    print "sending", filename
-    os.write(risc_input, chr(REC))
-    os.write(risc_input, filename+chr(0))
+def sendFile((risc_input, risc_output), verbose, filename):
+    if verbose:
+        print "sending", filename
+    send(risc_input, chr(REC))
+    send(risc_input, filename+chr(0))
     f=open(filename)
     s=f.read() # .replace("\012","\015")
     expectAck(risc_output)
     while len(s)>BlkLen:
-        os.write(risc_input, chr(BlkLen))
-        os.write(risc_input, s[:BlkLen])
+        send(risc_input, chr(BlkLen))
+        send(risc_input, s[:BlkLen])
         s=s[BlkLen:]
         expectAck(risc_output)
-    os.write(risc_input, chr(len(s)))
-    os.write(risc_input, s)
+    send(risc_input, chr(len(s)))
+    send(risc_input, s)
     expectAck(risc_output)
 
-def receiveFile((risc_input, risc_output), filename):
-    print "receiving", filename
-    os.write(risc_input, chr(SND))
-    os.write(risc_input, filename+chr(0))
+def receiveFile((risc_input, risc_output), verbose, filename):
+    if verbose:
+        print "receiving", filename
+    send(risc_input, chr(SND))
+    send(risc_input, filename+chr(0))
     x=receive(risc_output)
     if x==NAK:
         raise exceptions.Exception("No such file", filename)
@@ -67,21 +86,20 @@ def receiveFile((risc_input, risc_output), filename):
         while l == BlkLen:
             for i in range(l):
                 b.append(chr(receive(risc_output)))
-            os.write(risc_input, chr(ACK))
+            send(risc_input, chr(ACK))
             l=receive(risc_output)
     
         for i in range(l):
             b.append(chr(receive(risc_output)))
-        os.write(risc_input, chr(ACK))
+        send(risc_input, chr(ACK))
         open(filename, "w").write("".join(b))
     except:
-        os.write(risc_input, chr(NAK))
+        send(risc_input, chr(NAK))
         raise
     
-
 def openFifos():
-    risc_input=os.open("risc-input", os.O_WRONLY | os.O_NONBLOCK)
-    risc_output=os.open("risc-output", os.O_RDONLY | os.O_NONBLOCK)
+    risc_input=os.open(RISC_SERIAL_INPUT, os.O_WRONLY | os.O_NONBLOCK)
+    risc_output=os.open(RISC_SERIAL_OUTPUT, os.O_RDONLY | os.O_NONBLOCK)
     return (risc_input, risc_output)
 
 class Usage(Exception):
@@ -94,19 +112,22 @@ def main(argv=None):
         argv = sys.argv
     try:
         try:
-            opts, args = getopt.getopt(argv[1:], "ho:v", ["help", "output="])
+            opts, args = getopt.getopt(argv[1:], "vh", ["verbose", "help"])
         except getopt.error, msg:
             raise Usage(msg)
 
         # option processing
+        verbose = False
         for option, value in opts:
-            if option == "-v":
+            if option in ("-v", "--verbose"):
                 verbose = True
             if option in ("-h", "--help"):
                 raise Usage(help_message)
-            if option in ("-o", "--output"):
-                output = value
 
+        if len(args)==0:
+            print >> sys.stderr, str(help_message)
+            return 3
+            
         direction=args[0]
         args=args[1:]
 
@@ -116,19 +137,15 @@ def main(argv=None):
 
     except Usage, err:
         print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
-        print >> sys.stderr, "\t for help use --help"
         return 2
-
-        
 
     io=openFifos()
     if len(args)>0:
         for f in args:
             if direction == "send":
-               sendFile(io,f)
+               sendFile(io,verbose,f)
             else:
-               receiveFile(io,f)
-
+               receiveFile(io,verbose,f)
 
 if __name__ == "__main__":
     sys.exit(main())
--- risc-serial.c
+++ risc-serial.c
@@ -1,15 +1,16 @@
 #include <stdbool.h>
 #include <stdint.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/uio.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 #include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 #include "risc-serial.h"
+
+#define RISC_SERIAL_INPUT "/tmp/risc-serial-input"
+#define RISC_SERIAL_OUTPUT "/tmp/risc-serial-output"
 
 struct Serial {
     bool rx_available;
@@ -22,17 +23,42 @@ struct Serial {
 struct Serial *serial_new() {
     struct Serial *serial = calloc(1, sizeof(*serial));
     serial->tx_ready = true;
-    serial->rx_available = false;  
-    serial->input = open("risc-input", O_RDONLY | O_NONBLOCK);
-    printf("serial->input = %x\n", serial->input);
-    serial->output = open("risc-output", O_RDWR | O_NONBLOCK); // setvbuf(serial->output, NULL, _IONBF, 0);
-    printf("serial->output = %x\n", serial->output);
+    serial->rx_available = false;
+    serial->input = open(RISC_SERIAL_INPUT, O_RDONLY | O_NONBLOCK);
+    if (serial->input == -1) {
+        if (mknod(RISC_SERIAL_INPUT, S_IFIFO | 0660, 0) == -1) {
+            fprintf(stderr, "Can't make named pipe \"%s\": %s\n", RISC_SERIAL_INPUT, strerror(errno));
+        }
+        serial->input = open(RISC_SERIAL_INPUT, O_RDONLY | O_NONBLOCK);
+        if (serial->input == -1) {
+            fprintf(stderr, "Can't open file \"%s\": %s\n", RISC_SERIAL_INPUT, strerror(errno));
+            exit(1);
+        }
+    }
+
+    serial->output = open(RISC_SERIAL_OUTPUT, O_RDWR | O_NONBLOCK);
+    if (serial->output == -1) {
+        if (mknod(RISC_SERIAL_OUTPUT, S_IFIFO | 0660, 0) == -1) {
+            fprintf(stderr, "Can't make named pipe \"%s\": %s\n", RISC_SERIAL_OUTPUT, strerror(errno));
+        }
+        serial->output = open(RISC_SERIAL_OUTPUT, O_RDWR | O_NONBLOCK);
+        if (serial->output == -1) {
+            fprintf(stderr, "Can't open file \"%s\": %s\n", RISC_SERIAL_OUTPUT, strerror(errno));
+            exit(1);
+        }        
+    }
     return serial;
 }
 
 void serial_free(struct Serial *serial) {
-    close(serial->input);
-    close(serial->output);
+    if (close(serial->input)==-1) {
+        fprintf(stderr, "Can't close file \"%s\": %s\n", RISC_SERIAL_INPUT, strerror(errno));
+        exit(1);
+    };
+    if (close(serial->output)==-1) {
+        fprintf(stderr, "Can't close file \"%s\": %s\n", RISC_SERIAL_OUTPUT, strerror(errno));
+        exit(1);        
+    };
     free(serial);
 }
 
@@ -53,7 +79,6 @@ uint32_t serial_receive(struct Serial *serial) {
     if (serial_stat(serial) & 1) { // char available
         serial->rx_available = false;
         recv = serial->ch;
-        printf("recv=%d\n", recv); fflush(stdout);
     } else {
         recv= -1;
     }
@@ -63,17 +88,13 @@ uint32_t serial_receive(struct Serial *serial) {
 
 
 void serial_send(struct Serial *serial, uint32_t value) {
-    printf("send %d\n", value); fflush(stdout);
     char ch=value;
     int err=write(serial->output, &ch, 1);
-    printf("done sending. Result=%d\n", err);
     if (err==-1) {
-        printf ("errno = %d\n", errno);
+        fprintf(stderr, "Can't send via serial line: \"%s\"\n", strerror(errno));
     }
     err=fsync(serial->output);
-    if (err!=0) {
-        printf ("fsync errno = %d\n", errno);
+    if (err==-1) {
+        fprintf(stderr, "Can't flush serial line: \"%s\"\n", strerror(errno));
     }
-    
-     fflush(stdout);
 }
